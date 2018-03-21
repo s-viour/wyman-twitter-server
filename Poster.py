033be0a78aa6912283faff3925c2472fcf46913c
matt wyatt <mwyatt1000@gmail.com>
2018-03-20 22:08:12 -0400

    Poster and Watcher classes

---
+++ Poster.py
@@ -0,0 +1,55 @@
+import requests
+
+
+class Poster:
+    def __init__(self, watcher, webhook):
+        self.watcher = watcher
+        self.url = webhook
+        self.text_enabled = True
+        self.images_enabled = True
+
+    # functions for enabling/disabling sending images and text to the webhook
+    def enable_text(self):
+        self.text_enabled = True
+
+    def enable_images(self):
+        self.images_enabled = True
+
+    def disable_text(self):
+        self.text_enabled = False
+
+    def disable_images(self):
+        self.images_enabled = False
+
+    # checks the assigned watcher for a new tweet, if so it sends it to the webhook
+    def send(self):
+        if not self.watcher.check():
+            print("watcher check failed")
+
+        text = self.watcher.get("text")
+        image = self.watcher.get("image")
+        if not text:
+            text = ""
+        if not image:
+            image = ""
+
+        data = None
+        if self.text_enabled and self.images_enabled:
+            print("both enabled")
+            data = {
+                "content": text + image
+            }
+
+        elif self.text_enabled and not self.images_enabled:
+            print("only text enabled")
+            data = {
+                "content": text
+            }
+
+        elif not self.text_enabled and self.images_enabled:
+            print("only images enabled")
+            data = {
+                "content": image
+            }
+        sent = requests.post(self.url, data)
+        print(sent)
---
+++ Watcher.py
@@ -0,0 +1,73 @@
+import json
+import tweepy
+
+
+class Watcher:
+    def __init__(self, user_name):
+        self.user_name = user_name
+        self.consumer_key = None
+        self.consumer_secret = None
+        # while the last tweet will be stored in a local file, last_tweet will hold it in memory
+        self.last_tweet = None
+        self.api = None
+
+        # load and set config variables
+        with open("./config.json", "r") as f:
+            config = json.loads(f.read())
+            self.consumer_key = config["consumer_key"]
+            self.consumer_secret = config["consumer_secret"]
+
+        # check whether consumer_key or consumer_secret is blank
+        if not self.consumer_key or not self.consumer_secret:
+            raise Exception("consumer key or consumer secret is blank!")
+
+        # validate the credentials with the twitter api
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        try:
+            self.api = tweepy.API(auth)
+        except tweepy.TweepError:
+            raise Exception("consumer key or consumer secret is invalid!")
+
+        # yeah i know i'm accessing a protected object, shut it
+        self.last_tweet = self.api.user_timeline(id=self.user_name, count=1, tweet_mode="extended")[0]._json
+
+    # returns True if a new tweet has been posted since the last check
+    def check(self):
+        tweet = self.api.user_timeline(id=self.user_name, count=1, tweet_mode="extended")[0]._json
+        if tweet != self.last_tweet:
+            self.last_tweet = tweet
+            return True
+        return False
+
+    # used to grab various parts of the latest recorded tweet
+    # value list: 'id', 'date', 'url', 'image', 'text'
+    def get(self, what):
+        if what == "id":
+            return self.last_tweet["id_str"]
+        elif what == "date":
+            return self.last_tweet["created_at"]
+        elif what == "url":
+            return self.last_tweet["entities"]["media"][0]["url"]
+        elif what == "image":
+            try:
+                image = self.last_tweet["entities"]["media"][0]["media_url_https"]
+            except KeyError:
+                image = False
+            return image
+        elif what == "text":
+            try:
+                text = self.last_tweet["full_text"]
+            except KeyError:
+                text = False
+            return text
+
+    # function for checking if the tweet has an image or any text attached
+    def has(self, what):
+        string = ""
+        if what == "image":
+            string = self.get("image")
+        elif what == "text":
+            string = self.get("text")
+        if string == "":
+            return False
+        return True
